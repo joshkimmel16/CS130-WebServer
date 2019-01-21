84c82cd5d4170aadeeaac075e7f9cc96ec98e546
Albert Li <albertli@cs130.org>
2019-01-21 04:16:49 +0000

    session will close after sending response to client
    Change-Id: Iebfaef5d8973288098d4acad8fb2f3d4f48f58f6

--- src/session.cc
+++ src/session.cc
@@ -90,10 +90,12 @@ void session::handle_write(const boost::system::error_code& error)
 {
     if (!error) 
     {
-      socket_.async_read_some(boost::asio::buffer(data_, max_length),
-         boost::bind(&session::handle_read, this,
-         boost::asio::placeholders::error,
-         boost::asio::placeholders::bytes_transferred));
+      // socket_.async_read_some(boost::asio::buffer(data_, max_length),
+      //    boost::bind(&session::handle_read, this,
+      //    boost::asio::placeholders::error,
+      //    boost::asio::placeholders::bytes_transferred));
+      socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_send);
+      socket_.close();
     }
     else 
     {
