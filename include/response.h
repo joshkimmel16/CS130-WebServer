d5da3a40c60cedbe7c9ed6fe1c9de63c7ec03b34
Josh Kimmel <josh.kimmel@cs130.org>
2019-01-21 08:49:46 -0800

    Committing a skeleton of what our response class should do (for now). Can use this as a starting point for further development.
    Change-Id: If1826e558e9d419912c4c332d4b897158a3855b3

--- include/response.h
+++ include/response.h
@@ -1,21 +1,32 @@
+//An HTTP Response
+
+#include <cstdlib>
+#include <iostream>
 #include <string>
-#include <utility>
+#include <unordered_map>
 
-
-class Response{
-	public:
-		  Response();
-
-		    std::string get_ok_response();
-		      std::string get_request_msg();
-
-		        void set_request_msg(std::string request);
-
-			  char* to_string() const;
-
-	private:
-			    std::pair <std::string, std::string> hdr_content_type {"Content-Type", "text/plain"};
-			      const std::string ok_response =  "HTTP/1.1 200 OK\r\n";
-			        char *request_msg[1024];
+class response
+{
+public:
+    //Methods
+    response (); //default constructor
+    response (unsigned int status_code, std::string body); //constructor overload
+    unsigned int get_status_code (); //retrieve status code
+    std::string get_header(std::string name); //retrieve a given header by name
+    std::string get_body (); //retrieve response body
+    void set_status_code (unsigned int sc); //set the status code
+    void set_header (std::string h_name, std::string h_value); //set a specific header
+    void set_body (std::string body); //set the body
+    char* generate_response (); //generate a proper HTTP response
+private:
+    //Methods
+    std::string generate_status_line (); //create the status line of the response
+    std::string generate_header_lines (); //create the header lines of the response
+    std::string get_status_word (unsigned int status); //get the status description
+    
+    //Attributes
+    unsigned int status_code_;
+    std::unordered_map<std::string, std::string> response_headers_;
+    std::string response_body_;
 };
 
--- src/response.cc
+++ src/response.cc
@@ -1,31 +1,104 @@
+//An HTTP Response
 
+#include <cstdlib>
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include "response.h"
 
-#include "resonse.h"
-
-
-Response::Response() {}
-
-std::string Response::get_ok_reponse(){
-	  return ok_response;
+//overriden constructor takes a status code and body
+response::response (unsigned int status_code, std::string body)
+{
+    status_code_ = status_code;
+    response_body_ = body;
 }
 
-std::string Response::get_request_msg(){
-	  return request_msg;
+//get status code
+unsigned int response::get_status_code ()
+{
+    return status_code_;
 }
 
-void Response::set_request_msg(std::string request){
-	  std::memcpy(request_msg, request, sizeof(request));
+//get a specific header by name
+std::string response::get_header (std::string name)
+{
+    std::unordered_map<std::string, std::string>::const_iterator found = response_headers_.find(name);
+    if (!(found == response_headers_.end()))
+    {
+        return found->second;
+    }
+    else
+    {
+        return "";
+    }
 }
-char* Response::to_string() const{
-	  std::string out_msg = get_ok_response();
-	    out_msg += hdr_content_type.first;
-	      out_msg += ":";
-	        out_msg += hdr_content_type.second;
-		  out_msg += "\r\n";
-		    out_msg += get_request_msg();
 
-		      return out_msg.c_str();
+//get response body
+std::string response::get_body ()
+{
+    return response_body_;
 }
 
+//set the status code
+void response::set_status_code (unsigned int sc)
+{
+    status_code_ = sc;
+}
+
+//set a specific header
+void response::set_header (std::string h_name, std::string h_value)
+{
+    std::pair<std::string, std::string> h (h_name, h_value);
+    response_headers_.insert(h);
+}
+
+//set the response body
+void response::set_body (std::string body)
+{
+    response_body_ = body;
+}
+
+//generate a well-formed HTTP response based on internal attributes
+//return the response as a C string so it can be written on a socket
+char* response::generate_response ()
+{
+    string output = "";
+    output += generate_status_line();
+    output += generate_header_lines();
+    output += response_body_;
+    
+    char* r = output.c_str();
+    return r;
+}
+
+//generate a well-formed HTTP response status line based on internal attributes
+std::string response::generate_status_line ()
+{
+    return "HTTP/1.1 " + std::to_string(status_code_) + " " + get_status_word(status_code_) + "\r\n";
+}
+
+//generate a well-formed HTTP response header line(s) based on internal attributes
+std::string response::generate_header_lines ()
+{
+    std::string output = "";
+    for (std::pair<std::string, std::string> header : response_headers_)
+    {
+	   output += (header.first + ": " + header.second + "\r\n");
+    }
+    return (output + "\r\n");
+}
+
+//return the status description corresponding to a given HTTP status
+std::string response::get_status_word (unsigned int status)
+{
+    switch (status)
+    {
+        case (200):
+            return "OK";
+        case (400):
+            return "Bad Request";
+        default:
+            return "";
+    }
+    return "";
+}
