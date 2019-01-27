a79335910c6ab312b9cdc2f64fe03513815cb835
Josh Kimmel <josh.kimmel@cs130.org>
2019-01-26 16:24:14 -0800

    Further updates to integration test. Now can test both good and bad requests. Still need to add the ability to verify the responses.
    Change-Id: I3dae521ea662bf97ce5d6daf522024994688181e

--- tests/integration.sh
+++ tests/integration.sh
@@ -2,19 +2,41 @@
 
 #function to print proper usage for the bash script
 print_usage() {
-  echo "Valid commands are: -h (help), -c PATH_TO_SERVER_CONFIG, -b (execute invalid request)"
+  STR=$'Valid commands are:\n-h help\n-c PATH TO SERVER CONFIG\n-n USE NETCAT INSTEAD OF CURL\n-p PORT THAT WEB SERVER IS RUNNING ON\n-m VALID REQUEST METHOD\n-b VALID REQUEST BODY\n-o PATH TO FILE CONTAINING THE EXPECTED OUTPUT FOR THE GIVEN REQUEST'
+  echo "$STR"
   exit 1
+}
+
+create_curl_request() {
+    if [ ! -z "$REQ_BODY" ]
+    then
+        REQ_COMMAND="curl -i -X ${REQ_METHOD} --data \"${REQ_BODY}\" http://localhost:${SERVER_PORT}"
+    else
+        REQ_COMMAND="curl -i -X ${REQ_METHOD} http://localhost:${SERVER_PORT}"
+    fi
+}
+
+create_nc_request() {
+    REQ_COMMAND="nc localhost ${SERVER_PORT}"
 }
 
 #variable declarations with default values
 TEST_DIR=$(pwd)
+CONFIG="Integration/integration_config"
+USE_NETCAT="0"
+
+REQ_COMMAND=""
+REQ_METHOD="GET"
+REQ_BODY=
+
 SERVER_PATH="$(pwd)/../build/bin/server_main"
-CONFIG="integration_config"
-REQ_TYPE=0
 SERVER_PID=
+SERVER_PORT="8080"
+
+EXPECTED_OUTPUT="Integration/expected_output"
 
 #capture arguments to the script
-while getopts "hc:b" opt; do
+while getopts "hc:np:m:b:o:" opt; do
   case $opt in
     h)
       print_usage
@@ -22,8 +44,20 @@ while getopts "hc:b" opt; do
     c)
       CONFIG=$OPTARG
       ;;
+    n)
+      USE_NETCAT="1"
+      ;;
+    p)
+      SERVER_PORT=$OPTARG
+      ;;
+    m)
+      REQ_METHOD=$OPTARG
+      ;;
     b)
-      REQ_TYPE=1
+      REQ_BODY=$OPTARG
+      ;;
+    o)
+      EXPECTED_OUTPUT=$OPTARG
       ;;
     \?)
       echo "Invalid option (-$OPTARG)"
@@ -34,7 +68,7 @@ done
 
 SERVER_EX="${SERVER_PATH} ${CONFIG}"
 
-#run the server and capture its PID
+#run the server (as a background process) and capture its PID
 $SERVER_EX &
 SERVER_PID=$!
 
@@ -42,14 +76,30 @@ SERVER_PID=$!
 if [ ! -z "$SERVER_PID" ]
 then
     echo "Server is running!"
-    #run test request
-    #check result
+    sleep 0.3
+    
+    #generate the appropriate request
+    #then execute the generated request
+    if [ "$USE_NETCAT" == "1" ]
+    then
+        create_nc_request
+        OUTPUT=$(echo $REQ_BODY | $REQ_COMMAND)
+        echo "$OUTPUT" &> "Integration/tmp"
+    else
+        create_curl_request
+        $($REQ_COMMAND &> Integration/tmp)
+    fi
+    
+    #diff the output with some expected result
+    #TODO: FIGURE THIS OUT
+    #DIFF=$(diff Integration/tmp $EXPECTED_OUTPUT)
+    
     #kill server process
+    #TODO: GETTING ERROR WHEN TRYING TO REMOVE TEMP FILE
     kill $SERVER_PID
 else
     echo "The web server failed to start!"
     exit 2
 fi
 
-echo "We made it!"
 exit 0
