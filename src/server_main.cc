b4bd9e31d791098fd212e9f6f22f237eeb379f81
sungmin.ahn <sungmin.ahn@cs130.org>
2019-02-01 13:35:22 -0800

    changed CMakeLists.txt to include Boost Log and added codes to server_main.cc to test out whether it creates a file and logs. IT definitely needs modification.
    Change-Id: I0a91a9d8012324d0c4bb732494a592b1ed229a15

--- src/server_main.cc
+++ src/server_main.cc
@@ -10,16 +10,61 @@
 //
 
 #include "server.h"
+#include <boost/log/trivial.hpp>
+#include "boost/log/utility/setup.hpp"
+
+//Code from :
+//https://blog.scalyr.com/2018/07/getting-started-quickly-c-logging/
+//
+//init_log is for testing only (for boost log and output file)
+static void init_log()
+{
+  static const std::string COMMON_FMT("[%TimeStamp%][%Severity%]:  %Message%");
+  boost::log::register_simple_formatter_factory< boost::log::trivial::severity_level, char >("Severity");
+  boost::log::add_console_log
+  (
+      std::cout,
+      boost::log::keywords::format = COMMON_FMT,
+      boost::log::keywords::auto_flush = true
+  );
+
+
+  boost::log::add_file_log (
+      boost::log::keywords::file_name = "sample_%3N.log",
+      boost::log::keywords::rotation_size = 1 * 1024 * 1024,
+      boost::log::keywords::max_size = 20 * 1024 * 1024,
+      boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0, 0, 0),
+      boost::log::keywords::format = COMMON_FMT,
+      boost::log::keywords::auto_flush = true
+  );
+
+  boost::log::add_common_attributes();
+
+#ifndef _DEBUG
+  boost::log::core::get()->set_filter(
+      boost::log::trivial::severity >= boost::log::trivial::info
+  );
+#endif
+
+}
+
 
 using boost::asio::ip::tcp;
 
 int main(int argc, char* argv[]) 
 {
+  //testing to see Boost log works///////////////////////////////
+  init_log();
+  BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
+  BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
+  BOOST_LOG_TRIVIAL(info) << "An informational severity message";
+  ///////////////////////////////////////////////////////////////
   try
   {
     if (argc != 2)
     {
       std::cerr << "Usage: async_tcp_echo_server <path to config file>\n";
+      BOOST_LOG_TRIVIAL(error);
       return 1;
     }
 
@@ -32,12 +77,13 @@ int main(int argc, char* argv[])
       return 1;
     } 
     int port = config.GetPort();
-
+    
     boost::asio::io_service io_service;
 
     server s(io_service, port);
 
     io_service.run();
+    BOOST_LOG_TRIVIAL(info) << "server runs successfullly";
   }
   catch (std::exception& e)
   {
