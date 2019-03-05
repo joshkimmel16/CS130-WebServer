176570562fa78f234132b1b6711ed73e8a89d28d
Albert Li <albertli@cs130.org>
2019-03-05 22:31:55 +0000

    added tests for list and some misc tests
    Change-Id: I2d52349676d7abfd376856ff4f13c771a53fc96f

--- tests/meme_handler_test.cc
+++ tests/meme_handler_test.cc
@@ -2,10 +2,12 @@
 #include <cstring>
 #include <iostream>
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "meme_handler.h"
 #include <unistd.h>
 #include <vector>
 
+using ::testing::HasSubstr;
 
 class MemeHandlerTest : public ::testing::Test {
   protected:
@@ -19,6 +21,117 @@ class MemeHandlerTest : public ::testing::Test {
       config = out_config.statements_[0]->child_block_->statements_[0]->child_block_;
     }
 };
+
+//add 3 new memes and verify meme list returns them correctly
+TEST_F(MemeHandlerTest, MemeListTest) {
+  std::string input = "POST /memes HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nContent-Type: application/x-www-form-urlencoded\r\nCache-Control: max-age=0\r\nContent-Length: 54\r\n\r\nmemeSelect=grumpy.jpg&topCaption=123&bottomCaption=123";
+  const int n = input.length();
+  char char_arr[n+1];
+  strcpy(char_arr, input.c_str());
+  req_char_arr = char_arr;
+  req_len = std::strlen(char_arr);
+  std::shared_ptr<request> req(new request(req_char_arr, req_len));
+
+  std::shared_ptr<route_handler> out = meme_handler::create_handler(config, "");
+  std::shared_ptr<response> res = out->handle_request(req);
+
+  std::string firstID = res->get_body().substr(7, 38);
+  
+  EXPECT_EQ(res->get_status_code(), 201);
+  EXPECT_EQ(res->get_header("Content-Type"), "application/json");
+
+  std::string input2 = "POST /memes HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nContent-Type: application/x-www-form-urlencoded\r\nCache-Control: max-age=0\r\nContent-Length: 54\r\n\r\nmemeSelect=simply.jpg&topCaption=123&bottomCaption=123";
+  const int n2 = input2.length();
+  char char_arr2[n2+1];
+  strcpy(char_arr2, input2.c_str());
+  req_char_arr = char_arr2;
+  req_len = std::strlen(char_arr2);
+  std::shared_ptr<request> req2(new request(req_char_arr, req_len));
+
+  std::shared_ptr<response> res2 = out->handle_request(req2);
+
+  std::string secondID = res2->get_body().substr(7, 38);
+  
+  EXPECT_EQ(res2->get_status_code(), 201);
+  EXPECT_EQ(res2->get_header("Content-Type"), "application/json");
+
+  std::string input3 = "POST /memes HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nContent-Type: application/x-www-form-urlencoded\r\nCache-Control: max-age=0\r\nContent-Length: 57\r\n\r\nmemeSelect=ermahgerd.jpg&topCaption=123&bottomCaption=123";
+  const int n3 = input3.length();
+  char char_arr3[n3+1];
+  strcpy(char_arr3, input3.c_str());
+  req_char_arr = char_arr3;
+  req_len = std::strlen(char_arr3);
+  std::shared_ptr<request> req3(new request(req_char_arr, req_len));
+
+  std::shared_ptr<response> res3 = out->handle_request(req3);
+
+  std::string thirdID = res3->get_body().substr(7, 38);
+  
+  EXPECT_EQ(res3->get_status_code(), 201);
+  EXPECT_EQ(res3->get_header("Content-Type"), "application/json");
+
+  std::string input4 = "GET /memes HTTP/1.1\r\n\r\n";
+  const int n4 = input4.length();
+  char char_arr4[n4+1];
+  strcpy(char_arr4, input4.c_str());
+  req_char_arr = char_arr4;
+  req_len = std::strlen(char_arr4);
+  std::shared_ptr<request> req4(new request(req_char_arr, req_len));
+
+  std::shared_ptr<response> res4 = out->handle_request(req4);
+
+  EXPECT_THAT(res4->get_body(), HasSubstr(firstID));
+  EXPECT_THAT(res4->get_body(), HasSubstr(secondID));
+  EXPECT_THAT(res4->get_body(), HasSubstr(thirdID));
+}
+
+//test that only post and get methods are valid
+TEST_F(MemeHandlerTest, InvalidMethodTest) {
+  std::string input = "DELETE /memes HTTP/1.1\r\n\r\n";
+  const int n = input.length();
+  char char_arr[n+1];
+  strcpy(char_arr, input.c_str());
+  req_char_arr = char_arr;
+  req_len = std::strlen(char_arr);
+  std::shared_ptr<request> req(new request(req_char_arr, req_len));
+
+  std::shared_ptr<route_handler> out = meme_handler::create_handler(config, "");
+  std::shared_ptr<response> res = out->handle_request(req);
+
+  EXPECT_EQ(res->get_status_code(), 405);
+  EXPECT_EQ(res->get_body(), "This route only supports the HTTP GET and POST methods!");
+}
+
+TEST_F(MemeHandlerTest, ViewRedirectTest) {
+  std::string input = "POST /memes HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nContent-Type: application/x-www-form-urlencoded\r\nCache-Control: max-age=0\r\nContent-Length: 54\r\n\r\nmemeSelect=grumpy.jpg&topCaption=123&bottomCaption=123";
+  const int n = input.length();
+  char char_arr[n+1];
+  strcpy(char_arr, input.c_str());
+  req_char_arr = char_arr;
+  req_len = std::strlen(char_arr);
+  std::shared_ptr<request> req(new request(req_char_arr, req_len));
+
+  std::shared_ptr<route_handler> out = meme_handler::create_handler(config, "");
+  std::shared_ptr<response> res = out->handle_request(req);
+
+  std::string firstID = res->get_body().substr(8, 36);
+  
+  EXPECT_EQ(res->get_status_code(), 201);
+  EXPECT_EQ(res->get_header("Content-Type"), "application/json");
+
+  std::string input2 = "GET /memes/view/" + firstID + " HTTP/1.1\r\n\r\n";
+  const int n2 = input.length();
+  char char_arr2[n2+1];
+  strcpy(char_arr2, input2.c_str());
+  req_char_arr = char_arr2;
+  req_len = std::strlen(char_arr2);
+  std::shared_ptr<request> req2(new request(req_char_arr, req_len));
+
+  std::shared_ptr<response> res2 = out->handle_request(req2);
+
+  EXPECT_EQ(res2->get_status_code(), 303);
+  EXPECT_EQ(res2->get_header("Location"), "/web/memes/view.html?id=" + firstID);
+}
 
 //Meme create good redirect test
 TEST_F(MemeHandlerTest, CreateBasicTest) {
@@ -191,5 +304,3 @@ TEST_F(MemeHandlerTest, CreateAndViewTest) {
   EXPECT_EQ(body.substr(top_loc, 5), "LLONG");
   EXPECT_EQ(body.substr(bot_loc, 11), "TOOOOO LONG");
 }
-
-
