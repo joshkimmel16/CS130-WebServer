c347c351caf0698b2c4c18622334f68ce464a498
Josh Kimmel <josh.kimmel@cs130.org>
2019-03-10 16:25:51 -0700

    Adding unit tests for updating and deleting memes (per assignment spec).
    Change-Id: Iffa923ab7e6e7fa612b550dd5e05472561cf74bb

--- tests/meme_handler_test.cc
+++ tests/meme_handler_test.cc
@@ -305,6 +305,169 @@ TEST_F(MemeHandlerTest, CreateAndViewTest) {
   EXPECT_EQ(body.substr(bot_loc, 11), "TOOOOO LONG");
 }
 
+TEST_F(MemeHandlerTest, GoodUpdateTest) {
+  std::string input = "POST /memes HTTP/1.1\r\n\r\nmemeSelect=grumpy.jpg&topCaption=123&bottomCaption=123";
+  const int n = input.length();
+  char char_arr[n+1];
+  strcpy(char_arr, input.c_str());
+  req_char_arr = char_arr;
+  req_len = std::strlen(char_arr);
+  std::shared_ptr<request> req(new request(req_char_arr, req_len));
+
+  std::shared_ptr<route_handler> out = meme_handler::create_handler(config, "");
+  std::shared_ptr<response> res = out->handle_request(req);
+  
+  EXPECT_EQ(res->get_status_code(), 201);
+    
+  std::string resp_body = res->get_body();
+  std::string id = "";
+  std::size_t id_loc = resp_body.find("id");
+  if(id_loc != std::string::npos)
+  {
+    id = resp_body.substr(id_loc + 6, 36);
+  }
+
+  EXPECT_NE(id, "");
+    
+  std::string new_request = "PUT /memes/";
+  new_request += id;
+  new_request += " HTTP/1.1\r\n\r\nmemeSelect=simply.jpg&topCaption=456&bottomCaption=789";
+    
+  const int n2 = input.length();
+  char char_arr2[n2 + 1];
+  strcpy(char_arr2, new_request.c_str());
+  req_char_arr = char_arr2;
+  req_len = std::strlen(char_arr2);
+  std::shared_ptr<request> req2(new request(req_char_arr, req_len));
+
+  std::shared_ptr<route_handler> out2 = meme_handler::create_handler(config, "");
+  std::shared_ptr<response> res2 = out->handle_request(req2);
+
+  EXPECT_EQ(res2->get_status_code(), 200);
+}
+
+TEST_F(MemeHandlerTest, BadUpdateTest) {
+  std::string input = "POST /memes HTTP/1.1\r\n\r\nmemeSelect=grumpy.jpg&topCaption=123&bottomCaption=123";
+  const int n = input.length();
+  char char_arr[n+1];
+  strcpy(char_arr, input.c_str());
+  req_char_arr = char_arr;
+  req_len = std::strlen(char_arr);
+  std::shared_ptr<request> req(new request(req_char_arr, req_len));
+
+  std::shared_ptr<route_handler> out = meme_handler::create_handler(config, "");
+  std::shared_ptr<response> res = out->handle_request(req);
+  
+  EXPECT_EQ(res->get_status_code(), 201);
+    
+  std::string resp_body = res->get_body();
+  std::string id = "";
+  std::size_t id_loc = resp_body.find("id");
+  if(id_loc != std::string::npos)
+  {
+    id = resp_body.substr(id_loc + 6, 36);
+  }
+
+  EXPECT_NE(id, "");
+    
+  std::string new_request = "PUT /memes/";
+  new_request += "absolutegarbage";
+  new_request += " HTTP/1.1\r\n\r\nmemeSelect=simply.jpg&topCaption=456&bottomCaption=789";
+    
+  const int n2 = input.length();
+  char char_arr2[n2 + 1];
+  strcpy(char_arr2, new_request.c_str());
+  req_char_arr = char_arr2;
+  req_len = std::strlen(char_arr2);
+  std::shared_ptr<request> req2(new request(req_char_arr, req_len));
+
+  std::shared_ptr<route_handler> out2 = meme_handler::create_handler(config, "");
+  std::shared_ptr<response> res2 = out->handle_request(req2);
+
+  EXPECT_EQ(res2->get_status_code(), 400);
+}
+
+TEST_F(MemeHandlerTest, GoodDeleteTest) {
+  std::string input = "POST /memes HTTP/1.1\r\n\r\nmemeSelect=grumpy.jpg&topCaption=123&bottomCaption=123";
+  const int n = input.length();
+  char char_arr[n+1];
+  strcpy(char_arr, input.c_str());
+  req_char_arr = char_arr;
+  req_len = std::strlen(char_arr);
+  std::shared_ptr<request> req(new request(req_char_arr, req_len));
+
+  std::shared_ptr<route_handler> out = meme_handler::create_handler(config, "");
+  std::shared_ptr<response> res = out->handle_request(req);
+  
+  EXPECT_EQ(res->get_status_code(), 201);
+    
+  std::string resp_body = res->get_body();
+  std::string id = "";
+  std::size_t id_loc = resp_body.find("id");
+  if(id_loc != std::string::npos)
+  {
+    id = resp_body.substr(id_loc + 6, 36);
+  }
+
+  EXPECT_NE(id, "");
+    
+  std::string new_request = "DELETE /memes/";
+  new_request += id;
+  new_request += " HTTP/1.1\r\n\r\n";
+    
+  const int n2 = input.length();
+  char char_arr2[n2 + 1];
+  strcpy(char_arr2, new_request.c_str());
+  req_char_arr = char_arr2;
+  req_len = std::strlen(char_arr2);
+  std::shared_ptr<request> req2(new request(req_char_arr, req_len));
+
+  std::shared_ptr<route_handler> out2 = meme_handler::create_handler(config, "");
+  std::shared_ptr<response> res2 = out->handle_request(req2);
+
+  EXPECT_EQ(res2->get_status_code(), 200);
+}
+
+TEST_F(MemeHandlerTest, BadDeleteTest) {
+  std::string input = "POST /memes HTTP/1.1\r\n\r\nmemeSelect=grumpy.jpg&topCaption=123&bottomCaption=123";
+  const int n = input.length();
+  char char_arr[n+1];
+  strcpy(char_arr, input.c_str());
+  req_char_arr = char_arr;
+  req_len = std::strlen(char_arr);
+  std::shared_ptr<request> req(new request(req_char_arr, req_len));
+
+  std::shared_ptr<route_handler> out = meme_handler::create_handler(config, "");
+  std::shared_ptr<response> res = out->handle_request(req);
+  
+  EXPECT_EQ(res->get_status_code(), 201);
+    
+  std::string resp_body = res->get_body();
+  std::string id = "";
+  std::size_t id_loc = resp_body.find("id");
+  if(id_loc != std::string::npos)
+  {
+    id = resp_body.substr(id_loc + 6, 36);
+  }
+
+  EXPECT_NE(id, "");
+    
+  std::string new_request = "DELETE /memes/";
+  new_request += "absolutegarbage";
+  new_request += " HTTP/1.1\r\n\r\n";
+    
+  const int n2 = input.length();
+  char char_arr2[n2 + 1];
+  strcpy(char_arr2, new_request.c_str());
+  req_char_arr = char_arr2;
+  req_len = std::strlen(char_arr2);
+  std::shared_ptr<request> req2(new request(req_char_arr, req_len));
+
+  std::shared_ptr<route_handler> out2 = meme_handler::create_handler(config, "");
+  std::shared_ptr<response> res2 = out->handle_request(req2);
+
+  EXPECT_EQ(res2->get_status_code(), 400);
+}
 
 //simple test if search function works
 TEST_F(MemeHandlerTest, SearchTest) {
